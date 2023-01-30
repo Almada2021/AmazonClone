9c315d63630ed619025691bdb74b017a47dd215e
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-01-30 00:40:24 +0000

    Api: first crud of prroduct

---
+++ src/product/product.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './product.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{name: 'Product', schema: ProductSchema}])
+  ],
+  controllers: [ProductController],
+  providers: [ProductService]
+})
+export class ProductModule {}
