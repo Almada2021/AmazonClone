ab8d1935e7a0703e26c3662e5b07db341f85ed4b
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-02-28 21:44:44 +0000

    change code style

--- src/product/product.schema.ts
+++ src/product/product.schema.ts
@@ -1,16 +1,15 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
-export type ProductDocument = Product & Document
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+export type ProductDocument = Product & Document;
 @Schema()
 export class Product {
-    @Prop({required: true})
-    name: string;
-    
-    @Prop({required: true})
-    price: number;
+  @Prop({ required: true })
+  name: string;
 
-    @Prop()
-    description: string;
+  @Prop({ required: true })
+  price: number;
 
+  @Prop()
+  description: string;
 }
 export const ProductSchema = SchemaFactory.createForClass(Product);
